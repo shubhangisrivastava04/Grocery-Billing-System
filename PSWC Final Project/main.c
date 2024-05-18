73175e45e2655f3155803bb73ddff3903f125ba6
shubhangisrivastava04 <139063188+shubhangisrivastava04@users.noreply.github.com>
2024-05-18 20:19:35 +0530

    Update main.c

--- PSWC Final Project/main.c
+++ PSWC Final Project/main.c
@@ -2,49 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-struct Item {
+struct Item 
+{
     char name[100];
     int quantity;
     float price;
 };
 
-int main() {
-
+int main() 
+{
     char name[100];
     char user_name[100];
-    int number; 
+    char number[10]; 
 
     printf("Welcome!\n");
     printf("Login:\n");
 
-    username(user_name);
+    login(user_name);
     strcpy(name, user_name); 
-    
-    char pin[50];
-    password(pin);
 
-    number = mob_no();
+    FILE *fp = fopen("inventory.csv", "r");
+    char line[500];
+    int row = 0; 
+    int column = 0;
+
+    mob_no(number);
 
     int option = 0;
-    printf("Please select an Option: \n");
-
-
+   
     struct Item *items = (struct Item *)malloc(100 * sizeof(struct Item));
     int num_items = 0;
 
-    while (option != 3) {
-        printf("1.Item Details Entry\n2.Bill all Items\n3.Exit\n");
+    while (option != 4) 
+    {
+        printf("-------MAIN MENU-------\n");
+        printf("Please select an Option: \n");
+        printf("1.Item Details Entry\n2.Bill all Items\n3.View Inventory\n4.Exit\n");
+        printf("-----------------------\n");
+
         printf("Enter your choice: \n");
         scanf("%d", &option);
+        printf("\n");
 
-        switch (option) {
+        switch (option) 
+        {
             case 1: 
+            
                 printf("Please Enter Item Details:\n");
-                item_name(items[num_items].name);
-                items[num_items].quantity = item_quantity(&items[num_items].quantity);
-                items[num_items].price = item_price(&items[num_items].price);
-                num_items++; 
+                int val=500;
+                val=item_entry(items, num_items);
+                if(val==0)
+                {
+                    break;
+                }
+                else
+                {
+                    num_items++;
+                }
                 break;
 
             case 2: 
@@ -52,15 +66,48 @@ int main() {
                 break;
 
             case 3:
-                printf("Exiting...\n");
+                printf("---------------------------------------\n");
+                printf("\t\tINVENTORY\n");
+                printf("---------------------------------------\n");
+                printf("ID\t\tProduct\t\tPrice\n");
+                printf("---------------------------------------\n");
+                
+                while(fgets(line, 500, fp))
+                {
+                    column = 0;
+                    row++;
+
+                    if (row==1)
+                        continue;
+
+                    char* val = strtok(line, ",");
+                    
+                    while(val)
+                    {
+                        printf("%s\t\t", val);
+                        val = strtok(NULL, ",");
+                        column++;
+                    }
+                    printf("\n");
+                }
+                printf("---------------------------------------\n\n");
+
+                fseek(fp, 0, SEEK_SET);
+                fgets(line, 500, fp);
+                break;
+
+            case 4:
+                printf("Exiting...\n\nThank You!\n\n");
                 break;
 
             default:
-                printf("Invalid option. Please choose a valid option.\n");
+                printf("Invalid option. Please choose a valid option.\n\n");
                 break;
         }
+        
     }    
-
+   
     free(items);
     return 0;
+
 }
