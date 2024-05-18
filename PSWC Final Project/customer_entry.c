c6dea8386a088abe7bb8c1dead837131220f04c7
shubhangisrivastava04 <139063188+shubhangisrivastava04@users.noreply.github.com>
2024-05-18 20:22:08 +0530

    Update customer_entry.c

--- PSWC Final Project/customer_entry.c
+++ PSWC Final Project/customer_entry.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
-// char customer_entry(char name[100])
-// {
-//     printf("Enter Customer Name: ");
-//     gets(name);
-//     return *name;
-// }
-
-int mob_no(int num)
+void mob_no(char num[10])
 {
     printf("Enter Customer Mobile No.: ");
-    scanf("%d",&num);
+    scanf("%s", num);
+    printf("\n");
     return num;
 }
